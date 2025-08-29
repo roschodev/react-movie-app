8849679f100027d39cf278ed1bf8ed31fd90be1e
Rosch.Dev <roscho.dev@gmail.com>
2025-08-29 09:14:02 +0200

    repair

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import MovieCard from "./components/MovieCard.jsx";
 import search from "./components/Search.jsx";
 import {useDebounce} from "react-use";
 import {getTrendingMovies, updateSearchCount} from "./appwrite.js";
+import SideBar from './components/SideBar.jsx';
+import { FaBuffer, FaBrain, FaCalendar, FaAddressBook, FaAddressCard } from 'react-icons/fa';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -17,6 +19,72 @@ const API_OPTIONS = {
         Authorization: `Bearer ${API_KEY}`
     }
 }
+//bg-color-number
+const globalIconBgColor = "bg-gray-900";
+//hover:bg-color-number
+const globalHoverColor = "hover:bg-gray-800";
+//text-color-number
+const globalIconColor = "text-purple-400";
+//bg-color-number
+const barColor = "bg-gray-900";
+//mt-number
+const iconSpacing = "mt-0";
+
+const iconSize = "w-12 h-12"; // Adjust icon size as needed
+
+const sizeMatch = iconSize.match(/w-(\d+)/);
+const baseSize = sizeMatch ? parseInt(sizeMatch[1], 10) : 6; // default fallback
+const dividedSize = baseSize / 3; // ➝ 2
+const dividedSizeRem = `${dividedSize * 0.25}rem`;  // e.g., 2 → 0.5rem
+
+const sidebarItems = [
+   { icon: <FaBuffer size={dividedSizeRem} />, 
+    tooltip: 'Bottom Icon', 
+    iconColor: globalIconColor, 
+    index: 0, 
+    groupIndex: 0, 
+    iconBgColor: globalIconBgColor, 
+    hoverColor: globalHoverColor,
+    iconSize: iconSize
+
+  },
+   { icon: <FaBrain size={dividedSizeRem}/>, 
+    tooltip: 'Bottom Icon', 
+    iconColor: globalIconColor, 
+    index: 1, 
+    groupIndex: 0, 
+    iconBgColor: globalIconBgColor, 
+    hoverColor: globalHoverColor,
+    iconSize: iconSize 
+  },
+   { icon: <FaCalendar size={dividedSizeRem} />, 
+    tooltip: 'Bottom Icon', 
+    iconColor: globalIconColor, 
+    index: 2, 
+    groupIndex: 0, 
+    iconBgColor: globalIconBgColor, 
+    hoverColor: globalHoverColor,
+    iconSize: iconSize
+  },
+  { icon: <FaAddressBook size={dividedSizeRem} />, 
+    tooltip: 'Bottom Icon', 
+    iconColor: globalIconColor, 
+    index: 0, 
+    groupIndex: 1, 
+    iconBgColor: globalIconBgColor, 
+    hoverColor: globalHoverColor,
+    iconSize: iconSize 
+  },
+  { icon: <FaAddressCard size={dividedSizeRem}/>, 
+    tooltip: 'Bottom Icon', 
+    iconColor: globalIconColor, 
+    index: 1, 
+    groupIndex: 1, 
+    iconBgColor: globalIconBgColor, 
+    hoverColor: globalHoverColor,
+    iconSize: iconSize
+  },
+];
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("")
@@ -26,7 +94,7 @@ const App = () => {
     const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
     const [trendingMovies, setTrendingMovies] = useState([])
 
-    useDebounce(() => setDebouncedSearchTerm(searchTerm), 250, [searchTerm])
+    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
     const fetchMovies = async ({query = ''}) => {
         setIsLoading(true);
@@ -72,7 +140,7 @@ const App = () => {
        loadTrendingMovies()
     },[]);
 
-
+    console.log(trendingMovies.length)
 
     return(
         <main>
@@ -81,14 +149,13 @@ const App = () => {
                 <header>
                     <img src="./hero-img.png" alt="Hero Banner" />
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-
                     <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
 
                 {trendingMovies.length > 0 && (
                     <section className="trending">
                         <h2>Trending Movies</h2>
-
+                        
                         <ul>
                             {trendingMovies.map((movie, index) => (
                                 <li key={movie.$id}>
@@ -102,7 +169,7 @@ const App = () => {
 
                 <section className="all-movies">
                     <h2>All Movies</h2>
-
+                   
                     {isLoading ? (
                         <Spinner />
                     ) : errorMessage ? (
